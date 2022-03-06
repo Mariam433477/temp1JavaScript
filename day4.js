d677a16848f3a46408f21f2eb941ac38febd40fa
Mariam433477 <https://github.com/Mariam433477>
2022-03-06 02:13:39 +0200

    create project

---
+++ day4.js
@@ -0,0 +1,10 @@
+var x=prompt("Enter your num1");
+var y=prompt("Enter your num2");
+if(x>y){
+    document.write(parseInt(x));
+}
+else if(x<y){
+    document.write(parseInt(y)); 
+}else{
+    document.write("equal");
+}
